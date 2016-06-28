a5bb2da5cb08f1c99a73acb2c381cb725bf2ba56
Willy Aguirre <marti1125@gmail.com>
2016-06-28 00:36:29 -0500

    show data from json in datatable

---
+++ js/app.js
@@ -0,0 +1,47 @@
+$(function() {
+
+  var table = $('#example').DataTable({
+    "aoColumns": [
+        { "data": "numDet" },
+        { "data": "fecHoraTransmisionDPO" },
+        { "data": "anioDep" },
+        { "data": "fecHoraArribo" },
+        { "data": "numDocPostal" },
+        { "data": "desTipoServicio" },
+        { "data": "estadoDPO" },
+        { "data": "destinacionAduanera" },
+        { "data": "puertoEmbarque" },
+        { "data": "remitente" },
+        { "data": "consignatario" },
+        { "data": "direccion" },
+        { "data": "ubigeo" },
+        { "data": "desMercancia" },
+        { "data": "totalBultosManifestado" },
+        { "data": "totalPesoManifestado" },
+        { "data": "fobVerificado" },
+        { "data": "observacion" }
+    ]
+  });
+
+  $('#btnResult').click(function(){
+
+    var params = {"hdd_opcionConsulta": "2", "sel_tipServicio": "", "tipoBusqueda": "2", "txt_NumeroPostal": "CD189036253JP", "qpages1": "5",
+                  "dojo.preventCache": "1467078518970"}
+
+    $.ajax({
+        url: "http://www.sunat.gob.pe/ol-ad-itimportafacilmc/DepConsulta.htm?action=consultarDepEstado",
+        type: "post",
+        data: params ,
+        success: function (response) {
+          var json = JSON.parse(response);
+          var data = json.resultado.lstRpta;
+          table.rows.add(data).draw();
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+           console.log(textStatus, errorThrown);
+        }
+    });
+
+  });
+
+});
